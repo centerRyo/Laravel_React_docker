8a34c4e78758cdf8ae8a468d81e8c69cdbb4386f
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-12-17 16:50:46 +0900

    ユーザー一覧画面を作成

--- server/resources/views/users/index.blade.php
+++ server/resources/views/users/index.blade.php
@@ -1,7 +1,28 @@
+@extends('layouts.app')
+@section('title', 'ユーザー一覧')
+@section('content')
 <div class="container">
     <div class="mt-2 row">
-        @foreach ($users as $user)
-        <p>{{ $user->name }}</p>
-        @endforeach
+        <table class="table table-striped">
+            <thead>
+                <tr class="table-info">
+                    <th scope="col">ID</th>
+                    <th scope="col">名前</th>
+                    <th scope="col">メールアドレス</th>
+                    <th scope="col">職業</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->job }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
+@endsection
